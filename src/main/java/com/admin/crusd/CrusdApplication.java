1544949f514dce004ca771d553007e31285504ae
Sothuon_Thorn <sothuonthorn@gmail.com>
2018-06-26 14:47:37 +0700

    test

---
+++ src/main/java/com/admin/crusd/CrusdApplication.java
@@ -0,0 +1,12 @@
+package com.admin.crusd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrusdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrusdApplication.class, args);
+    }
+}
